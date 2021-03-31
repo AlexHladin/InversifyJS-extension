18f9e7bec40669c0ac427cf0b0b3daa794582bf2
Oleksandr Hladin <alexgladin17@gmail.com>
2021-03-31 10:40:50 +0300

    Update index.ts file

--- src/container/container_extension.ts
+++ src/container/container_extension.ts
@@ -2,7 +2,7 @@ import { Container, interfaces } from 'inversify';
 import { BindingToSyntaxExtension } from '../syntax/binding_to_syntax_extension';
 
 export class ContainerExtension extends Container {
-  public bind<T>(serviceIdentifier: interfaces.ServiceIdentifier<T>): interfaces.BindingToSyntax<T> {
+  public bind<T>(serviceIdentifier: interfaces.ServiceIdentifier<T>): BindingToSyntaxExtension<T> {
     const service = super.bind(serviceIdentifier);
     
     return new BindingToSyntaxExtension(service);
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from './container/container_extension';
+export * from './interfaces/interfaces';
+export * from './syntax/binding_to_syntax_extension';
